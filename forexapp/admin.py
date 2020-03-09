1ccb34806010ae933760064fa706deea6dcc5e7a
julesjo <57871149+julesjo@users.noreply.github.com>
2020-03-08 22:53:48 -0400

    updated

--- forexapp/admin.py
+++ forexapp/admin.py
@@ -1,8 +1,14 @@
 from django.contrib import admin
 from .models import ForexCurrent
+from .models import ForexHistoric
 # Register your models here.
 
 class ForexCurrentAdmin(admin.ModelAdmin):
     list_display = ('Date', 'AUD','EUR','NZD','GBP','BRL','CAD','CNY','HKD','INR','KRW','MXN','ZAR')
 
 admin.site.register(ForexCurrent, ForexCurrentAdmin)
+
+class ForexHistoricAdmin(admin.ModelAdmin):
+    list_display = ('Date', 'AUD','EUR','NZD','GBP','BRL','CAD','CNY','HKD','INR','KRW','MXN','ZAR')
+
+admin.site.register(ForexHistoric, ForexHistoricAdmin)
